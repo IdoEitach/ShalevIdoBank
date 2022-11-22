c33355bbed70a2987ac39d2b3be01c6089626da5
IdoEitach <112800537+IdoEitach@users.noreply.github.com>
2022-11-22 09:34:43 +0200

    data connection

---
+++ BLL/AccountService.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+
+namespace ShalevIdoBank.BLL
+{
+    public class AccountService
+    {
+        static public double GetBalance(int accCode)
+        {
+            return DAL.AccountServiceDal.GetBalance(accCode);
+        }
+        static public DataTable GetTransactions(int accCode)
+        {
+            return DAL.AccountServiceDal.GetTransactions(accCode);
+
+        }
+        static public bool PayThatBill(int accCode)
+        {
+            return DAL.AccountServiceDal.PayThatBill(accCode);
+
+        }
+    }
+}
--- DAL/AccountServiceDal.cs
+++ DAL/AccountServiceDal.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 
@@ -7,6 +9,35 @@ namespace ShalevIdoBank.DAL
 {
     public class AccountServiceDal
     {
+        private SqlConnection connection;
+        // create public constructor
+        public AccountServiceDal()
+        {
+            this.connection = new SqlConnection(SQLHelper.connectionString);
+            this.connection.Open();
+        }
+
+        public void Dispose()
+        {
+            this.connection.Close();
+        }
+
+        public double GetBalance(int accId)
+        {
+            SqlCommand cmd = new SqlCommand("GetBalance", connection);
+            cmd.Parameters.Add("@accountId", SqlDbType.Int).Value = accId;
+            cmd.CommandType = CommandType.StoredProcedure;
+            double res = (double)cmd.ExecuteScalar();
+            return res;
+        }
+        static public DataTable GetTransactions(int accCode)
+        {
+            throw new NotImplementedException();
+        }
+        static public bool PayThatBill(int accCode)
+        {
+            throw new NotImplementedException();
+        }
 
     }
 }
