6b00296af8debdbe2ab89c871fb0c45f36f6bc95
student <student@mosilab.local>
2023-01-04 09:06:17 +0200

    initial forms

---
+++ FormsFrontend/Form1.Designer.cs
@@ -0,0 +1,62 @@
+﻿namespace FormsFrontend
+{
+  partial class Form1
+  {
+    /// <summary>
+    /// Required designer variable.
+    /// </summary>
+    private System.ComponentModel.IContainer components = null;
+
+    /// <summary>
+    /// Clean up any resources being used.
+    /// </summary>
+    /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+    protected override void Dispose(bool disposing)
+    {
+      if (disposing && (components != null))
+      {
+        components.Dispose();
+      }
+      base.Dispose(disposing);
+    }
+
+    #region Windows Form Designer generated code
+
+    /// <summary>
+    /// Required method for Designer support - do not modify
+    /// the contents of this method with the code editor.
+    /// </summary>
+    private void InitializeComponent()
+    {
+      this.lbl_title = new System.Windows.Forms.Label();
+      this.SuspendLayout();
+      // 
+      // lbl_title
+      // 
+      this.lbl_title.AutoSize = true;
+      this.lbl_title.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
+      this.lbl_title.Location = new System.Drawing.Point(331, 82);
+      this.lbl_title.Name = "lbl_title";
+      this.lbl_title.Size = new System.Drawing.Size(147, 55);
+      this.lbl_title.TabIndex = 0;
+      this.lbl_title.Text = "Pasta";
+      // 
+      // Form1
+      // 
+      this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+      this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+      this.ClientSize = new System.Drawing.Size(800, 450);
+      this.Controls.Add(this.lbl_title);
+      this.Name = "Form1";
+      this.Text = "Form1";
+      this.ResumeLayout(false);
+      this.PerformLayout();
+
+    }
+
+    #endregion
+
+    private System.Windows.Forms.Label lbl_title;
+  }
+}
+
