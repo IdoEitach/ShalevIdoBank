ea813f24b1c23757a5d09d7feba8c485a2eb1ace
IdoEitach <112800537+IdoEitach@users.noreply.github.com>
2023-02-09 14:18:55 +0200

    changes

--- BLL/AccountService.cs
+++ BLL/AccountService.cs
@@ -10,63 +10,65 @@ using System.Web;
 
 namespace ShalevIdoBank.BLL
 {
-  public class AccountService
-  {
-    static public double GetBalance(int accountId)
+    public class AccountService
     {
-      var service = new AccountServiceDal();
-      var res = service.GetBalance(accountId);
-      service.Dispose();
-      return res;
-    }
-    static public DataTable GetTransactions(int accountId)
-    {
-      var service = new AccountServiceDal();
-      var res = service.GetTransactions(accountId);
-      service.Dispose();
-      return res;
-    }
-    static public bool PayThatBill(float amount, string description, string payingUserName, string payingPassword, string payeeUserName)
-    {
-      var service = new AccountServiceDal();
-      int payingAccountId = service.GetAccountIdByUserName(payingUserName);
-      int payeeAccountId = service.GetAccountIdByUserName(payeeUserName);
-
-      if (AccountService.GetBalance(payingAccountId) < amount) return false;
-      bool res = service.PayThatBill(amount, description, payingAccountId, payeeAccountId);
-
-
-      // Send an Email
-      if (amount >= 100_000)
-      {
-        string from = "bank@gmail.com";
-        string to = service.GetAccountEmail(payingAccountId);
-        MailMessage message = new MailMessage(from, to);
-
-        string mailbody = "Your account has been billed a total of " + amount.ToString() + " money.";
-        message.Subject = "Warning! Your bank account has been billed more than 100,000 money.";
-        message.Body = mailbody;
-        message.BodyEncoding = Encoding.UTF8;
-        message.IsBodyHtml = true;
-        SmtpClient client = new SmtpClient("smtp.gmail.com", 587); // Gmail smtp    
-        System.Net.NetworkCredential basicCredential1 = new
-        System.Net.NetworkCredential("yourmail id", "Password");
-        client.EnableSsl = true;
-        client.UseDefaultCredentials = false;
-        client.Credentials = basicCredential1;
-        try
+        static public double GetBalance(int accountId)
         {
-          client.Send(message);
+            var service = new AccountServiceDal();
+            var res = service.GetBalance(accountId);
+            service.Dispose();
+            return res;
         }
-        catch (Exception ex)
+        static public DataTable GetTransactions(int accountId)
         {
-          // Couldn't send Email.
-          throw ex;
+            var service = new AccountServiceDal();
+            var res = service.GetTransactions(accountId);
+            service.Dispose();
+            return res;
         }
-      }
+        static public bool PayThatBill(float amount, string description, string payingUserName, string payingPassword, string payeeUserName)
+        {
+            var service = new AccountServiceDal();
+            if (!service.ValidateUser(payingUserName, payingPassword)) return false;
 
-      service.Dispose();
-      return res;
+            int payingAccountId = service.GetAccountIdByUserName(payingUserName);
+            int payeeAccountId = service.GetAccountIdByUserName(payeeUserName);
+
+            if (AccountService.GetBalance(payingAccountId) < amount) return false;
+            bool res = service.PayThatBill(amount, description, payingAccountId, payeeAccountId);
+
+
+            // Send an Email
+            if (amount >= 100_000)
+            {
+                string from = "bank@gmail.com";
+                string to = service.GetAccountEmail(payingAccountId);
+                MailMessage message = new MailMessage(from, to);
+
+                string mailbody = "Your account has been billed a total of " + amount.ToString() + " money.";
+                message.Subject = "Warning! Your bank account has been billed more than 100,000 money.";
+                message.Body = mailbody;
+                message.BodyEncoding = Encoding.UTF8;
+                message.IsBodyHtml = true;
+                SmtpClient client = new SmtpClient("smtp.gmail.com", 587); // Gmail smtp    
+                System.Net.NetworkCredential basicCredential1 = new
+                System.Net.NetworkCredential("yourmail id", "Password");
+                client.EnableSsl = true;
+                client.UseDefaultCredentials = false;
+                client.Credentials = basicCredential1;
+                try
+                {
+                    client.Send(message);
+                }
+                catch (Exception ex)
+                {
+                    // Couldn't send Email.
+                    throw ex;
+                }
+            }
+
+            service.Dispose();
+            return res;
+        }
     }
-  }
 }
--- DAL/AccountServiceDal.cs
+++ DAL/AccountServiceDal.cs
@@ -4,109 +4,122 @@ using System.Data;
 using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
+using System.Web.DynamicData;
 
 namespace ShalevIdoBank.DAL
 {
-  public class AccountServiceDal
-  {
-    private SqlConnection connection;
-    private SqlTransaction transaction;
-
-    // create public constructor
-    public AccountServiceDal()
+    public class AccountServiceDal
     {
-      this.connection = new SqlConnection(Database.connectionString);
-      this.connection.Open();
-      this.transaction = null;
+        private SqlConnection connection;
+        private SqlTransaction transaction;
+
+        // create public constructor
+        public AccountServiceDal()
+        {
+            this.connection = new SqlConnection(Database.connectionString);
+            this.connection.Open();
+            this.transaction = null;
+        }
+
+        public void Dispose()
+        {
+            this.connection.Close();
+        }
+
+        public string GetAccountEmail(int accountId)
+        {
+            SqlCommand cmd = new SqlCommand("spGetAccountEmail", connection, transaction);
+            cmd.Parameters.Add("@accountId", SqlDbType.Int).Value = accountId;
+            cmd.CommandType = CommandType.StoredProcedure;
+            string res = (string)cmd.ExecuteScalar();
+            return res;
+        }
+
+        public float GetBalance(int accountId)
+        {
+            SqlCommand cmd = new SqlCommand("spRetrieveBalance", connection, transaction);
+            cmd.Parameters.Add("@accountId", SqlDbType.Int).Value = accountId;
+            cmd.CommandType = CommandType.StoredProcedure;
+            float res = Convert.ToSingle(cmd.ExecuteScalar()); // Convert to float
+            return res;
+        }
+        public bool ValidateUser(string accountUserName, string password)
+        {
+            DataTable dataTable = new DataTable();
+            SqlCommand cmd = new SqlCommand("spValidateUser", connection, transaction);
+            cmd.Parameters.Add("@UserName", SqlDbType.NVarChar).Value = accountUserName;
+            cmd.Parameters.Add("@Password", SqlDbType.NVarChar).Value = password;
+            cmd.CommandType = CommandType.StoredProcedure;
+            SqlDataReader reader = cmd.ExecuteReader();
+            //load the data table
+            dataTable.Load(reader);
+            return dataTable.Rows.Count > 0;
+        }
+        public int GetAccountIdByUserName(string accountUserName)
+        {
+            SqlCommand cmd = new SqlCommand("spGetAccountIdByUsername", connection, transaction);
+            cmd.Parameters.Add("@accountUserName", SqlDbType.NVarChar).Value = accountUserName;
+            cmd.CommandType = CommandType.StoredProcedure;
+            int res = (int)cmd.ExecuteScalar();
+            return res;
+        }
+        public void UpdateBalance(int accountId, float newBalance)
+        {
+            SqlCommand cmd = new SqlCommand("spUpdateBalance", connection, transaction);
+            cmd.Parameters.Add("@accountId", SqlDbType.Int).Value = accountId;
+            cmd.Parameters.Add("@newBalance", SqlDbType.Int).Value = newBalance;
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.ExecuteNonQuery();
+
+        }
+
+        public void InsertTransaction(float amount, string description, int payingAccountId, int payeeAccountId)
+        {
+            SqlCommand cmd = new SqlCommand("spInsertTransaction", connection, transaction);
+            cmd.Parameters.Add("@date", SqlDbType.NVarChar).Value = DateTime.Now.ToString();
+            cmd.Parameters.Add("@amount", SqlDbType.Float).Value = amount;
+            cmd.Parameters.Add("@description", SqlDbType.NVarChar).Value = description;
+            cmd.Parameters.Add("@payingAccountId", SqlDbType.Int).Value = payingAccountId;
+            cmd.Parameters.Add("@payeeAccountId", SqlDbType.Int).Value = payeeAccountId;
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.ExecuteNonQuery();
+
+        }
+
+        public DataTable GetTransactions(int accountId)
+        {
+            DataTable dataTable = new DataTable();
+            SqlCommand cmd = new SqlCommand("spGetTransactions", connection, transaction);
+            cmd.Parameters.Add("@payingAccountId", SqlDbType.Int).Value = accountId;
+            cmd.CommandType = CommandType.StoredProcedure;
+            SqlDataReader reader = cmd.ExecuteReader();
+            //load the data table
+            dataTable.Load(reader);
+            return dataTable;
+        }
+
+        public bool PayThatBill(float amount, string description, int payingAccountId, int payeeAccountId)
+        {
+            transaction = connection.BeginTransaction();
+            try
+            {
+                InsertTransaction(amount, description, payingAccountId, payeeAccountId);
+
+                UpdateBalance(payingAccountId, GetBalance(payingAccountId) - amount);
+                UpdateBalance(payeeAccountId, GetBalance(payeeAccountId) + amount);
+
+                transaction.Commit();
+            }
+            catch (Exception)
+            {
+                transaction.Rollback();
+                return false;
+            }
+            finally
+            {
+                transaction = null;
+            }
+            return true;
+        }
     }
-
-    public void Dispose()
-    {
-      this.connection.Close();
-    }
-
-    public string GetAccountEmail(int accountId)
-    {
-      SqlCommand cmd = new SqlCommand("spGetAccountEmail", connection, transaction);
-      cmd.Parameters.Add("@accountId", SqlDbType.Int).Value = accountId;
-      cmd.CommandType = CommandType.StoredProcedure;
-      string res = (string)cmd.ExecuteScalar();
-      return res;
-    }
-
-    public float GetBalance(int accountId)
-    {
-      SqlCommand cmd = new SqlCommand("spRetrieveBalance", connection, transaction);
-      cmd.Parameters.Add("@accountId", SqlDbType.Int).Value = accountId;
-      cmd.CommandType = CommandType.StoredProcedure;
-      float res = Convert.ToSingle(cmd.ExecuteScalar()); // Convert to float
-      return res;
-    }
-    public int GetAccountIdByUserName(string accountUserName)
-    {
-      SqlCommand cmd = new SqlCommand("spGetAccountIdByUsername", connection, transaction);
-      cmd.Parameters.Add("@accountUserName", SqlDbType.NVarChar).Value = accountUserName;
-      cmd.CommandType = CommandType.StoredProcedure;
-      int res = (int)cmd.ExecuteScalar();
-      return res;
-    }
-    public void UpdateBalance(int accountId, float newBalance)
-    {
-      SqlCommand cmd = new SqlCommand("spUpdateBalance", connection, transaction);
-      cmd.Parameters.Add("@accountId", SqlDbType.Int).Value = accountId;
-      cmd.Parameters.Add("@newBalance", SqlDbType.Int).Value = newBalance;
-      cmd.CommandType = CommandType.StoredProcedure;
-      cmd.ExecuteNonQuery();
-
-    }
-
-    public void InsertTransaction(float amount, string description, int payingAccountId, int payeeAccountId)
-    {
-      SqlCommand cmd = new SqlCommand("spInsertTransaction", connection, transaction);
-      cmd.Parameters.Add("@date", SqlDbType.NVarChar).Value = DateTime.Now.ToString();
-      cmd.Parameters.Add("@amount", SqlDbType.Float).Value = amount;
-      cmd.Parameters.Add("@description", SqlDbType.NVarChar).Value = description;
-      cmd.Parameters.Add("@payingAccountId", SqlDbType.Int).Value = payingAccountId;
-      cmd.Parameters.Add("@payeeAccountId", SqlDbType.Int).Value = payeeAccountId;
-      cmd.CommandType = CommandType.StoredProcedure;
-      cmd.ExecuteNonQuery();
-
-    }
-
-    public DataTable GetTransactions(int accountId)
-    {
-      DataTable dataTable = new DataTable();
-      SqlCommand cmd = new SqlCommand("spGetTransactions", connection, transaction);
-      cmd.Parameters.Add("@accountId", SqlDbType.Int).Value = accountId;
-      cmd.CommandType = CommandType.StoredProcedure;
-      SqlDataReader reader = cmd.ExecuteReader();
-      //load the data table
-      dataTable.Load(reader);
-      return dataTable;
-    }
-
-    public bool PayThatBill(float amount, string description, int payingAccountId, int payeeAccountId)
-    {
-      transaction = connection.BeginTransaction();
-      try
-      {
-        InsertTransaction(amount, description, payingAccountId, payeeAccountId);
-
-        UpdateBalance(payingAccountId, GetBalance(payingAccountId) - amount);
-        UpdateBalance(payeeAccountId, GetBalance(payeeAccountId) + amount);
-
-        transaction.Commit();
-      }
-      catch (Exception)
-      {
-        transaction.Rollback();
-        return false;
-      }
-      finally
-      {
-        transaction = null;
-      }
-      return true;
-    }
-  }
 }
